0f19dd55c5f91e152cf601a4a8d30b4c53e3a13a
bicup <scr3am1@gmail.com>
2017-04-11 23:41:01 +0300

    asd

--- post-deploy.sh
+++ post-deploy.sh
@@ -5,5 +5,6 @@ export STORAGE_PATH=/home/curator/storage
 export MPD_PORT=15601
 export API_URL=localhost:15002/api/
 
-npm install && pm2 startOrRestart ecosystem.config.js --env production 
+npm install
+pm2 startOrRestart ecosystem.config.js --env production 
 exit
